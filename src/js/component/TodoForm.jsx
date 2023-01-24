25bcb484de3763e55a9e104675455a23da9c4211
Pas0ta <ssantamariarodrigo@gmail.com>
2023-01-24 17:53:40 +0000

    version final

--- src/js/component/TodoForm.jsx
+++ src/js/component/TodoForm.jsx
@@ -11,14 +11,14 @@ export default function TodoForm(props) {
   }
 
   return (
-    <form onSubmit={handleSubmit} className="todo-form">
+    <form onSubmit={handleSubmit} className="todo-form ">
       <input
         value={input}
         onChange={(e) => setInput(e.target.value)}
         className="todo-input"
         placeholder="Add a todo"
       />
-      <button type="submit" className="todo-button">Add Todo</button>
+      <button type="submit" className="todo-button">Añadir</button>
     </form>
   );
 }
